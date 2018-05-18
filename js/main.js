e5c89d76a46ad48372f218813efd30d5f2f2001a
Xiaochen Wang <xwang345@sy-sycc-33.local>
2018-05-18 10:26:46 -0400

    Q1-1

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 // Student Name: Xiaochen Wang Student ID: 015297153
+console.log("Hello");
+// function appendText(){
+//     console.log("addElement");
+//     var Element1 = "<li> Frist Name: <b> Xiaochen </b></li>";
+//     var Element2 = "<li> Last Name: <b> Wang </b></li>";
+//     var Element3 = "<li> Course Title: <b> WEB422 </b></li>";
 
-function appendText(){
-    var Element1 = "<b> Xiaochen </b>";
-    var Element2 = "<b> Wang </b>";
-    var Element3 = "<b> WEB422 </b>";
+//     var addElement = document.createElement("li");
+//     txt3.innerHTML = "Text.";
+//     $("body").append(Element1,Element2,Element3); 
+// }
 
-    var addElement = document.createElement("p");
-    txt3.innerHTML = "Text.";
-    $("body").append(Element1,Element2,Element3); 
-}
+$("ul#list > li").each(function(index){
+    console.log("addElement");
+    $(this).prepend('<b>' + index + ' - ' );
+});
