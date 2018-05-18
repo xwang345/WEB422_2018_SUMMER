2535e62d261bbcbcb2d6a48cc08f05fda1311c19
Xiaochen Wang <xwang345@sy-sycc-33.local>
2018-05-18 10:03:58 -0400

    Q1

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Student Name: Xiaochen Wang Student ID: 015297153
+
+function appendText(){
+    var Element1 = "<b> Xiaochen </b>";
+    var Element2 = "<b> Wang </b>";
+    var Element3 = "<b> WEB422 </b>";
+
+    var addElement = document.createElement("p");
+    txt3.innerHTML = "Text.";
+    $("body").append(Element1,Element2,Element3); 
+}
